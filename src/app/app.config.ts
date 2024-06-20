e1453db37342f38a28505b85775c78104df2ea0f
Alice Buchholz <alice.buchholz@gmail.com>
2024-06-20 11:56:34 +0200

    translation setup + scss unpdate

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,28 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { TranslateService } from '@ngx-translate/core';
+import { HttpLoaderFactory } from '../main';
+
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient()]
+  providers: [provideRouter(routes),
+    provideHttpClient(),
+    TranslateService,
+    importProvidersFrom (
+      HttpClientModule,
+      TranslateModule.forRoot({
+        defaultLanguage: "en",
+        loader: {
+          provide: TranslateLoader,
+          useFactory: HttpLoaderFactory,
+          deps: [HttpClient],
+        }
+      })
+    )
+  ]
 };
